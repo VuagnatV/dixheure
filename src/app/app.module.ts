2f3ef2ac34ad4ff6193ce8745a1a6dd96a0eda25
Nicolas Floris <floris.nicolas@outlook.fr>
2023-12-15 21:26:17 +0100

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HomeComponent } from './home/home.component';
+import { AuthComponent } from './auth/auth.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule,
